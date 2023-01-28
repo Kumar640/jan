b1d2a984d040e4939925b839f43c23a3a921196c
Kumar640 <pappukmr640@gmail.com>
2023-01-28 21:50:02 +0530

    Division Program

---
+++ DivisionCal.java
@@ -0,0 +1,23 @@
+package ecommece;
+
+import java.util.Scanner;
+
+public class DivisionCal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Max Marks");
+		int marks=sc.nextInt();
+		System.out.println("Enter Student Obtained Marks");
+		int i=sc.nextInt();
+		if(marks<=i)
+		{
+			System.out.println("First Division");
+		}
+			
+
+	}
+
+}
