5e0cd624eb6e3d9095e0d42c05db9d1304bcc6e2
Kumar640 <pappukmr640@gmail.com>
2023-01-28 21:41:23 +0530

    Area Program

---
+++ Area.java
@@ -0,0 +1,14 @@
+package ecommece;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float radious=9.2f;
+		float pie=2.14f;
+		double result=pie*radious*radious;
+		System.out.println("Area of circle is: "+ String.format("%.2f", result));
+
+	}
+
+}
