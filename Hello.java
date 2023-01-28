53f72ce00425bed2ce049c0f58d5cd0a93bee5ec
Kumar640 <pappukmr640@gmail.com>
2023-01-28 21:44:31 +0530

    Hello

---
+++ Hello.java
@@ -0,0 +1,20 @@
+package ecommece;
+
+public class Hello {
+
+	public static void main(String[] pappu) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int i=5;
+		System.out.println("Value of i="+i);
+		float f=1.5f;
+		System.out.println(f);
+		char ch='A';
+		System.out.println(ch);
+		boolean ret=true;
+		System.out.println(ret);
+		
+		
+	}
+
+}
